713cc8273b2f568468c897a2dcb7e902e6ac2f80
Frank J. Gómez <frank@pawpawworks.com>
2018-12-02 14:25:37 -0500

    Allowed user to specify a file to which scraped data should be written instead of dumping it to the console.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const baseUrl = 'https://www.dischord.com/fugazi_live_series';
 
 // Includes
+const fs = require('fs');
 const program = require('commander');
 const puppeteer = require('puppeteer');
 
@@ -12,6 +13,8 @@ program
     console.log('');
     console.log("Fugotcha is a command-line utility for scraping data from the Fugazi Live Series on Dischord.com.");
   })
+  .option('-o --output <file>',
+    'Required. The name of the file to which to output the scraped data.')
   .option('-p --page <slug>',
     'Required. The slug of the page to scrape (the URL after "fugazi_live_series")')
   .option('-c --count [count]',
@@ -19,6 +22,7 @@ program
   .action(function () {
     const slug = validatePage(program.page);
     const pageLimit = validateCount(program.count);
+    const outputStream = createOutputStream(program.output);
 
     puppeteer.launch().then(async browser => {
       const page = await browser.newPage();
@@ -29,8 +33,7 @@ program
       do {
         tracks = await extractTracks(page);
         releaseId = await extractReleaseId(page);
-        console.log(tracks.join(','));
-        console.log(releaseId);
+        outputStream.write([releaseId, tracks.join(',')].join(',') + "\n");
 
         morePagesToScrape = (++i < pageLimit) || (pageLimit === 0);
 
@@ -46,6 +49,7 @@ program
         }
       } while (morePagesToScrape);
 
+      outputStream.close();
       await browser.close();
     });
   })
@@ -89,6 +93,30 @@ function extractTracks(page) {
         return track.innerText.trim();
       });
     });
+  });
+}
+
+/**
+ * Creates a writeStream from the provided filename.
+ *
+ * @param {String} value
+ * @return {<fs.WriteStream>}
+ */
+function createOutputStream(filename) {
+  if (typeof(filename) === 'undefined') {
+    console.error('Output is a required parameter.');
+    process.exit(1);
+  }
+
+  return fs.createWriteStream(filename, {flags: 'wx'}).on('error', e => {
+    switch (e.code) {
+      case 'EEXIST':
+        console.error(`File ${filename} already exists; aborting so as not to overwrite.`);
+        break;
+      default:
+        console.error(e.message);
+    }
+    process.exit(1);
   });
 }
 
